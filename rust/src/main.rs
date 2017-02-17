56a01e28b1f1c01ae1ffd9194947e44d04baf098
Nam Chu Hoai <nambrot@googlemail.com>
2017-02-17 15:12:07 -0500

    Try it on a larger file

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,15 +1,19 @@
 extern crate rand;
 use rand::Rng;
+use std::io::prelude::*;
+use std::fs::File;
 mod inversions;
 
 
 fn main() {
-    let arr : Vec<u32> = vec![0; rand::thread_rng().gen_range(1, 10)]
-                .into_iter()
-                .map(|_x| rand::thread_rng().gen_range(1, 100))
-                .collect();
+    let mut s = String::new();
+    let mut file = File::open("../raws/num_of_inversions_in_array.txt").unwrap();
+    file.read_to_string(&mut s).unwrap();
 
+    let arr: Vec<u32> =
+        s.split_whitespace()
+            .map(|string: &str| string.parse::<u32>().unwrap())
+            .collect();
     println!("{:?}", arr);
     println!("{:?}", inversions::count(&arr));
-
 }
